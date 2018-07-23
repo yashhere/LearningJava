8288cf58eb94805f26ec883cd444ce08d167b49c
Yash Agarwal <yashagarwaljpr@gmail.com>
2018-07-23 20:09:00 +0530

    working till now

--- src/main/java/api/ProductData.java
+++ src/main/java/api/ProductData.java
@@ -1,49 +1,80 @@
 package main.java.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductData {
-	private int productId;
-	private String productName;
-	private String type;
-	private int productQuantity;
-	private int productPrice;
-
-	public int getProductId() {
-		return productId;
+	List<ProductTemplate> products = new ArrayList<>();
+	
+	public ProductData() {
+		ProductTemplate product=new ProductTemplate();
+		product.setProductId(1);
+		product.setProductName("Veg McMuffin");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		product=new ProductTemplate();
+		product.setProductId(2);
+		product.setProductName("Veg Supreme McMuffin");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		product=new ProductTemplate();
+		product.setProductId(3);
+		product.setProductName("McEgg");
+		product.setProductPrice(50);
+		product.setType(1);
+		products.add(product);
+		
+		product=new ProductTemplate();
+		product.setProductId(4);
+		product.setProductName("Egg & Cheese McMuffin");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		
+		product=new ProductTemplate();
+		product.setProductId(5);
+		product.setProductName("Sausage McMuffin");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		product=new ProductTemplate();
+		product.setProductId(6);
+		product.setProductName("Sausage McMuffin with Egg");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		
+		product=new ProductTemplate();
+		product.setProductId(7);
+		product.setProductName("McAloo Tikki");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		
+		product=new ProductTemplate();
+		product.setProductId(8);
+		product.setProductName("McVeggie");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
+		
+		product=new ProductTemplate();
+		product.setProductId(9);
+		product.setProductName("McSpicy Paneer");
+		product.setProductPrice(50);
+		product.setType(0);
+		products.add(product);
 	}
-
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public int getProductQuantity() {
-		return productQuantity;
-	}
-
-	public void setProductQuantity(int productQuantity) {
-		this.productQuantity = productQuantity;
-	}
-
-	public int getProductPrice() {
-		return productPrice;
-	}
-
-	public void setProductPrice(int productPrice) {
-		this.productPrice = productPrice;
+	
+	public List<ProductTemplate> getProducts() {
+		return this.products;
 	}
 }
--- src/main/java/api/Statistics.java
+++ src/main/java/api/Statistics.java
@@ -29,4 +29,48 @@ public class Statistics {
 		}
 		return listOfCustomers;
 	}
+
+	@Path("/getStatisticOnOrderProduct")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<ProductTemplate> getStatisticOnOrderProduct() {
+		List<ProductTemplate> productsList = new ArrayList<>();
+		try {
+			List<CustomerOrder> orders = CustomerOrdersList.customerOrders;
+
+			ProductData productData = new ProductData();
+			List<ProductTemplate> products = productData.getProducts();
+
+			int productQuantities[];
+			productQuantities = new int[products.size()];
+
+			for (int i = 0; i < products.size(); i++) {
+				productQuantities[i] = 0;
+			}
+
+			for (CustomerOrder customerOrder : orders) {
+				for (Order order : customerOrder.getOrders()) {
+					for (ProductTemplate product : products) {
+						if (product.getProductId() == order.getProductId()) {
+							productQuantities[product.getProductId() - 1] += order.getProductQuantity();
+						}
+					}
+				}
+			}
+
+			for (int i=0;i<productQuantities.length;i++) {
+				System.out.println(productQuantities[i]);
+			}
+			
+			for (ProductTemplate product : products) {
+				product.setProductQuantity(productQuantities[product.getProductId() - 1]);
+//				System.out.println(product.getProductQuantity(productQuantities[product.getProductId()-1]));
+				productsList.add(product);
+			}
+			return productsList;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return productsList;
+	}
 }
